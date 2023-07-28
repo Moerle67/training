9c2f3ce640bf5879f7b78ad1a335e3af93c94a3a
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-07-28 12:36:56 +0200

    anmeldung

--- app1/urls.py
+++ app1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.start, name = "start"),
+    path('anmeldung', views.anmeldung, name = "anmeldung"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def start(request):
+    return render(request, "app1/basis.html")
+
+def anmeldung(request):
+    return render(request, "app1/anmeldung.html")
